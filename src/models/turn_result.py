9b2391d1dde8486935ba1503cfc12909c3dc7569
mouse <miccro@gmail.com>
2026-03-12 01:12:44 +0300

    fix

--- src/models/turn_result.py
+++ src/models/turn_result.py
@@ -1,14 +1,8 @@
-from __future__ import annotations
-
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
 
 from src.models.common import CellState
 from src.models.ship import Ship
 from src.models.turn import Turn
-
-if TYPE_CHECKING:
-    from src.models.player import Player
 
 
 @dataclass(frozen=True)
